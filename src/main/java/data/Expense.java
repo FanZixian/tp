7a0954c7b954036856f17bbadbbc62ca7b2fd05a
Zixian Fan <98700507+FanZixian@users.noreply.github.com>
2023-03-15 23:16:39 +0800

    Update Expense.java

--- src/main/java/data/Expense.java
+++ src/main/java/data/Expense.java
@@ -1,28 +1,25 @@
 package data;
 
-import java.math.RoundingMode;
 import java.util.Objects;
-import java.math.BigDecimal;
 
 public class Expense {
-    protected BigDecimal expenseAmount;
+    protected double expenseAmount;
     protected Time expenseTime;
     protected String description;
     protected Currency currencyType;
 
-    public Expense(BigDecimal expenseAmount, Time expenseTime, String description, Currency currencyType) {
-        this.expenseAmount = formatExpenseAmount(expenseAmount);
+    public Expense() {
+        this.expenseAmount = 0;
+    }
+
+    public Expense(double expenseAmount, Time expenseTime, String description, Currency currencyType) {
+        this.expenseAmount = expenseAmount;
         this.expenseTime = expenseTime;
         this.description = description;
         this.currencyType = currencyType;
     }
 
-    private BigDecimal formatExpenseAmount(BigDecimal originalExpenseAmount) {
-        BigDecimal roundedExpense = originalExpenseAmount.setScale(2, RoundingMode.HALF_UP);
-        return roundedExpense;
-    }
-
-    public BigDecimal getExpenseAmount() {
+    public double getExpenseAmount() {
         return expenseAmount;
     }
 
@@ -39,7 +36,7 @@ public class Expense {
     }
 
     // The setter method will be used if the User want to change some information in their previous expense
-    public void setExpenseAmount(BigDecimal expenseAmount) {
+    public void setExpenseAmount(double expenseAmount) {
         this.expenseAmount = expenseAmount;
     }
 
@@ -60,7 +57,7 @@ public class Expense {
             return true;
         }
         return Objects.equals(this.getDescription(), ((Expense) obj).getDescription())
-                && this.getExpenseAmount().equals(((Expense) obj).getExpenseAmount())
+                && this.getExpenseAmount() == ((Expense) obj).getExpenseAmount()
                 && Objects.equals(this.getExpenseTime(), ((Expense) obj).getExpenseTime())
                 && Objects.equals(this.getCurrencyType(), ((Expense) obj).getCurrencyType());
     }
@@ -70,13 +67,40 @@ public class Expense {
      */
     @Override
     public String toString() {
-        // Currently not useful
         String currencyString = Currency.returnCurrency(this.currencyType);
-        String amountString = this.expenseAmount.toString();
+        String amountString = Double.toString(this.expenseAmount);
         String descriptionString = this.description;
         String timeString = this.expenseTime.toString();
         return (currencyString + amountString + " cat:" + descriptionString + " date:" + timeString);
     }
 
+    public void printExpense() {
+        String currencyString = Currency.returnCurrency(this.currencyType);
+        String descriptionString = this.description;
+        String timeString = this.expenseTime.toString();
+        System.out.print(currencyString);
+        printExpenseAmount();
+        System.out.println(" cat:" + descriptionString + " date:" + timeString);
+    }
+
+    /**
+     * To standardize the printed expense amount to be 2 decimal places
+     * If the number is integer or only 1 digit, then no change
+     */
+    public void printExpenseAmount() {
+        String expenseAmountString = Double.toString(expenseAmount);
+        int digitNumbers = expenseAmountString.length() - expenseAmountString.indexOf(".") - 1;
+        if (digitNumbers == 1) {
+            // No digit
+            if (expenseAmountString.endsWith("0")) {
+                System.out.print(expenseAmountString.substring(0,expenseAmountString.length() - 2));
+            } else {
+                System.out.print(expenseAmountString);
+            }
+        } else {
+            double expenseAmountTwoDecimal = Math.round(this.expenseAmount * 100.0) / 100.0;
+            System.out.printf("%.2f", expenseAmountTwoDecimal);
+        }
+    }
 }
 
